0c82ae16399b65dab7aa80fc31114b1d8be31c64
prem-18 <63889908+prem-18@users.noreply.github.com>
2020-10-04 11:18:53 +0530

    Update Vinay.py

--- Vinay.py
+++ Vinay.py
@@ -8,8 +8,7 @@ def timed(label):
     # Setup -__enter__ 
     print(f"{label}: Start at {start}") 
     try: yield 
-    # yield to body of `with` statement 
-    finally: # Teardown - __exit__ 
+    
         end = time() 
     print(f"{label}: End at {end} ({end - start} elapsed)")
     
